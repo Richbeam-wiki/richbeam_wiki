5a06455bf4907b3013bb10c23ee84f5498e31db3
Richbeam_wiki <15621333364@163.com>
2023-07-20 17:50:51 +0800

    first commit

---
+++ build/html/_sources/introduction/production.rst.txt
@@ -0,0 +1,17 @@
+锐驰智光（北京）科技有限公司（Richbeam）成立于2016年。
+
+产品
+==========
+
+新产品介绍
+------------
+
+Lakibeam
+~~~~~~~~~~~~~~~~~~~~~~
+Lakibeam 系列由 Richbeam 团队~~~。点击了解详情：
+
+`Lakibeam1 <https://www.richbeam.com/lakibeam1>`_
+
+`Lakibeam1L <https://www.richbeam.com/lakibeam1l>`_
+
+`Lakibeam1S <https://www.richbeam.com/lakibeam1s>`_
---
+++ source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'richbeam_wiki_cn'
+copyright = '2023, Richbeam'
+author = 'Richbeam'
+release = 'v0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+master_doc = 'index'
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
